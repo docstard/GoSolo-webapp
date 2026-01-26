63d878a25a9b5bec31cc9860f9c7c0daffbcd8f5
Dhruv Choudhary <dhruvchoudhary369@gmail.com>
2026-01-26 16:10:55 +0530

    add to cart save btn bug

--- components/QuantityPopover.tsx
+++ components/QuantityPopover.tsx
@@ -83,6 +83,7 @@ export function DrawerDialogDemo({ children, productQuantity, setProductQuantity
                             +
                         </button>
                     </div>
+                    <Button onClick={() => handleAddToCart(product, finalPrice)}>Add to cart</Button>
                 </DialogContent>
             </Dialog>
         )
